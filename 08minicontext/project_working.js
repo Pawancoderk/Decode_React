e27840529f8a02d2fca1477a3a2e81a5b661ddc3
Pawan <imrealpawan@gmail.com>
2025-06-24 14:51:13 +0530

    context api project

---
+++ 08minicontext/project_working.js
@@ -0,0 +1,2 @@
+// ! first we create createContext()
+// ! second we create usercontextprovider() and pass values like user , setUser and inside usercontextprovider()  we give value access to its children 
